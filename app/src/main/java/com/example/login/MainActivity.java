7be56f677fc7bc4e22320d35351d969a23a2347c
jenhk03 <114376127+jenhk03@users.noreply.github.com>
2023-02-22 09:29:37 +0700

    Update

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    LoginControl LC = new LoginControl();
+    private TextView tvSession;
+    private String takeSession;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if (!LC.isLogin(MainActivity.this, "spPadang"))
+        {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            finish();
+        }
+        else
+        {
+            setContentView(R.layout.activity_main);
+            takeSession = LC.getPref(MainActivity.this, "spPadang");
+            tvSession = findViewById(R.id.tv_session);
+            tvSession.setText(takeSession);
+        }
+    }
+    public void logoutProcess(View v)
+    {
+        LC.setPref(MainActivity.this, "spPadang", null);
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        finish();
     }
 }
